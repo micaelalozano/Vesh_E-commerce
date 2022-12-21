df3b52abb90ebb76bebc297aa0ccec3acf1ea2a9
micaelalozano <micaelalozano.16@hotmail.com>
2022-12-20 21:37:38 -0300

    New Navbar

--- src/components/HeadDos.jsx
+++ src/components/HeadDos.jsx
@@ -7,10 +7,9 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { blueGrey } from "@mui/material/colors";
 
-const HeadDos= () => {
-  return (
-    <>
-      <div className="contenedor-dos">
+const HeadDos = () => {
+  return <>
+   <div className="contenedor-dos">
         <ul className="header-menu-dos">
           <li>
             <Link to="/mis_favoritos">
@@ -29,9 +28,7 @@ const HeadDos= () => {
           </li>
         </ul>
       </div>
-    </>
-  );
+  </>;
 };
 
 export default HeadDos;
-
--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import NavDos from "../components/NavDos";
 import HeadDos from "../components/HeadDos";
-//import NavDos from "../components/NavDos";
+import Footer from "../components/Footer";
+//Estilos
+import "../estilos/favoritos.css";
 
 const Favoritos = () => {
   return (
     <>
       <HeadDos />
+      <NavDos />
+      <div className="car-contenedor">
+        <div className="car-contenedor1">
+          <h3 className="hijo">
+            <span className="icon1">favorite</span>
+            <strong> TUS FAVORITOS </strong>
+            <span className="icon">favorite</span>
+          </h3>
+          <p className="hijo2"> No tienes favoritos.</p>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
