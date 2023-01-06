2ea6af572dd458067d01884fc0cf068a97e85090
micaelalozano <micaelalozano.16@hotmail.com>
2023-01-06 12:42:49 -0300

    Deploy

--- src/pages/PantalonesList.jsx
+++ src/pages/PantalonesList.jsx
@@ -18,6 +18,9 @@ const PantalonesList = () => {
       .then((res) => res.data)
       .then((productos) => {
         setProductos(productos);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
 
